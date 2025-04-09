d7f77c3546a502e916448526204fc01271613f24
Jazz2016 <jazminalaniscuellar@gmail.com>
2025-04-09 17:10:14 +0200

    Update Conexion.py

--- Conexion.py
+++ Conexion.py
@@ -3,20 +3,18 @@ import pymysql
 def conexionDB():
     try:
         miConexion = pymysql.connect(
-            host = "localhost", user="root", passwd="", db="login"
+            host="localhost", user="root", passwd="vistaalegre", db="login"
         )
-
         cur = miConexion.cursor()
         print("Conexión exitosa a la base de datos")
         return miConexion, cur
-   except pymysql.error as e:
-        print(f"Error al conectar a la base de daros: {e}")
+    except pymysql.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
         return None, None
 
- def cerrarConexion(miConexion):
-
+def cerrarConexion(miConexion):
     if miConexion:
         miConexion.close()
         print("Conexión cerrada correctamente")
     else:
-        print("No hay conexión activa para cerrar") 
+        print("No hay conexión activa para cerrar")
