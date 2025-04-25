8dab995ee96f931562fd48a8fcc8f643afc322d2
Jazz2016 <jazminalaniscuellar@gmail.com>
2025-04-25 15:42:39 +0200

    login6

---
+++ mainwindow.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import QLabel, QGraphicsDropShadowEffect, QApplication, QMainWindow
+from PyQt5.QtGui import QColor
+from PyQt5 import uic
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Login6.ui", self)  # tu archivo .ui
+
+        # Acceder al QLabel
+        label = self.findChild(QLabel, "labelTitulo")
+
+        # Crear efecto de sombra
+        shadow = QGraphicsDropShadowEffect()
+        shadow.setBlurRadius(10)
+        shadow.setColor(QColor(0, 0, 0, 160))  # negro semi-transparente
+        shadow.setOffset(2, 2)
+
+        # Aplicar al QLabel
+        label.setGraphicsEffect(shadow)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
